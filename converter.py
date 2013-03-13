9ae4c10770bdd87064a926e939a28bd94e61fb47
lpankaz <lpankaz@gmail.com>
2013-03-13 18:41:47 +0100

    Mocki klasy konwertujacej

--- converter.py
+++ converter.py
@@ -7,7 +7,16 @@ class Font:
 class FontLoader:
     def loadFont(self, directory):
         pass
-	
+
+class TextDrawer:
+    def setFont(self, font):
+        pass
+
+    def draw(self, text):
+        pass
+
+
+
 t = getText()
 f = FontLoader().loadFont('fancyFont/')
 d = TextDrawer()
